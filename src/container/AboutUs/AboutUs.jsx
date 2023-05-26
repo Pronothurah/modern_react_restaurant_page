3137f77f24d48a7d6c69ee4d38c025b3c3282d32
Pronothurah <onsongojosphat28@gmail.com>
2023-05-26 15:18:10 +0300

    navbar, header, aboutus and spexcial menu pages

--- src/container/AboutUs/AboutUs.jsx
+++ src/container/AboutUs/AboutUs.jsx
@@ -1,10 +1,32 @@
 import React from 'react';
 
+import { images } from '../../constants';
 import './AboutUs.css';
 
 const AboutUs = () => (
-  <div>
-    AboutUs
+  <div className='app__aboutus app__bg flex__center section__padding' id='about'>
+    <div className='app__aboutus-overlay flex__center'>
+      <img src={images.G} alt="g letter" />
+    </div>
+
+    <div className='app__aboutus-content flex__center'>
+      <div className='app__aboutus-content_about'>
+        <h1 className='headtext__cormorant'>About Us</h1>
+        <img src={images.spoon}  alt="about__spoon" className='spoon_img' />
+        <p className='p__opensans'>Indulge your senses in a symphony of flavors, where locally sourced ingredients are transformed into culinary wonders that push the boundaries of taste</p>
+        <button type='button' className='custom__button'>Know More</button>
+      </div>
+      <div className='app__aboutus-content_knife flex__center'>
+        <img src={images.knife} alt="about_knife" />
+      </div>
+      <div className='app__aboutus-content_history'>
+        <h1 className='headtext__cormorant'>Our History</h1>
+        <img src={images.spoon}  alt="about__spoon" className='spoon_img' />
+        <p className='p__opensans'>From our succulent seafood delicacies that transport you to the coastal shores to our tender cuts of prime meat that melt in your mouth, every bite tells a story of culinary excellence.</p>
+        <button type='button' className='custom__button'>Know More</button>
+      </div>
+    </div>
+
   </div>
 );
 
--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,11 +1,26 @@
 import React from 'react';
 
+import {SubHeading} from '../../components';
+import {images } from '../../constants';
 import './Header.css';
 
 const Header = () => (
-  <div>
-    Header
-  </div>
+    <div className='app__header section__padding app__wrapper' id='home'>
+      <div className='app__wrapper_info'>
+        <SubHeading title='Chase the new flavour' />
+        <h1 className='app__header-h1'>The Key to Fine Dining</h1>
+        <p className='p__opensans' style={{ margin: '2rem 0'}}>
+        Welcome to our modern culinary haven, where taste meets innovation and ambiance reigns supreme. 
+        Step into a world where delectable flavors are meticulously crafted, each dish an artful masterpiece
+         that delights both the eyes and the palate.
+        </p>
+        <button type='button' className='custom__button'>Explore More</button>
+      </div>
+      <div className='app__wrapper_img'>
+        <img src={images.welcome} alt="header img" />
+      </div>
+    </div>
+ 
 );
 
 export default Header;
