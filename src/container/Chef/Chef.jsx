0bbab63dad63ccd55e67473d07207baaaabae0c7
Pronothurah <onsongojosphat28@gmail.com>
2023-06-08 11:44:34 +0300

    Special Menu and Chef Sections

--- src/container/Chef/Chef.jsx
+++ src/container/Chef/Chef.jsx
@@ -1,10 +1,43 @@
 import React from 'react';
 
+import { SubHeading } from '../../components';
+import { images } from '../../constants';
+
 import './Chef.css';
 
 const Chef = () => (
-  <div>
-    Chef
+  <div className='app__bg app__wrapper section__padding'>
+    <div className="app__wrapper_img app__wrapper_img-reverse">
+      <img src={images.chef} alt="chef" />
+    </div>
+
+    <div className="app__wrapper_info">
+      <SubHeading title='Chef’s Word'/>
+      <h1 className='headtext__cormorant'>What we believe in</h1>
+
+      <div className="app__chef-content">
+        <div className="app__chef-content_quote">
+          <img src={images.quote} alt="quote" />
+          <p className='p__opensans'>
+          Let your taste buds embark on an exquisite culinary journey through the rich tapestry of 
+          Chinese flavors. 
+          </p>
+        </div>
+        <p className='p__opensans'>
+        Today, our chef's word of the day is 'Harmony.' 
+          In every dish we create, we strive to harmonize the vibrant colors, fragrant aromas, and 
+          bold flavors that define Chinese cuisine
+          </p>
+      </div>
+
+      <div className="app__chef-sign">
+        <p>Kevin Luo</p>
+        <p className='p__opensans'>Chef & Founder</p>
+        <img src={images.sign} alt="sign" />
+      </div>
+    </div>
+
+
   </div>
 );
 
